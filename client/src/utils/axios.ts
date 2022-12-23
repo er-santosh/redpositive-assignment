b3f54196ed5db62e49c2ebe008d3129c8a426ef7
ER Santosh <er.santoshtharu@gmail.com>
2022-12-23 13:13:26 +0545

    client api url changed

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api/";
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.BASE_URL
+    : "http://localhost:8000/api/";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
